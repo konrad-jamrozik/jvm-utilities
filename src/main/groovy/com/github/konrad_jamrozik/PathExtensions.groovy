e9e8c141a5a0520c653ef09c9f72c520bc90deb2
Konrad Jamrozik <spawarotti@jamro.pl>
2015-12-11 18:52:33 +0100

    fix headers

--- src/main/groovy/com/github/konrad_jamrozik/PathExtensions.groovy
+++ src/main/groovy/com/github/konrad_jamrozik/PathExtensions.groovy
@@ -1,11 +1,5 @@
-// Copyright (c) 2013-2015 Saarland University Software Engineering Chair.
-// All right reserved.
+// Author: Konrad Jamrozik, github.com/konrad-jamrozik
 //
-// Author: Konrad Jamrozik, jamrozik@st.cs.uni-saarland.de
-//
-// This file is part of the "DroidMate" project.
-//
-// www.droidmate.org
 package com.github.konrad_jamrozik
 
 import org.apache.commons.io.FilenameUtils
