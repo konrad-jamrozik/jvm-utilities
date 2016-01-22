f7076e6f4d24cae1480baf58c05e1636d29c1d1c
Konrad Jamrozik <spawarotti@jamro.pl>
2016-01-22 10:51:52 +0100

    remove breaking @TypeChecked annotation

--- src/test/groovy/com/github/konrad_jamrozik/ResourcePathTest.groovy
+++ src/test/groovy/com/github/konrad_jamrozik/ResourcePathTest.groovy
@@ -1,13 +1,11 @@
 package com.github.konrad_jamrozik
 
-import groovy.transform.TypeChecked
 import org.junit.FixMethodOrder
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 import org.junit.runners.MethodSorters
 
-@TypeChecked
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @RunWith(JUnit4)
 public class ResourcePathTest
