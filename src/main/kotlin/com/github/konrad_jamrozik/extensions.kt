d001752ba3356c63a83eeb1cc9a97a43f59ef90f
Konrad Jamrozik <spawarotti@jamro.pl>
2016-04-13 12:50:19 +0200

    merge kotlinUtilities project into utilities

---
+++ src/main/kotlin/com/github/konrad_jamrozik/extensions.kt
@@ -0,0 +1,32 @@
+package com.github.konrad_jamrozik
+
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+
+fun Path.resolveRegularFile(file: String): Path
+{
+  check(Files.isDirectory(this), {"Files.isDirectory($this)"})
+  check(file.length > 0, {"file.length > 0"})
+  
+  val resolvedFile = this.resolve(file)
+  
+  check(Files.isRegularFile(resolvedFile), {"Files.isRegularFile($resolvedFile)"})
+  
+  return resolvedFile
+}
+
+fun String.asEnvironmentDir(): Path
+{
+  val value = System.getenv(this)
+  
+  checkNotNull(value, {"System.getenv($this) should denote a directory. It is instead null" })
+  check(value.length > 0, {"System.getenv($this) should denote a directory. It is instead an empty string."})
+  
+  val dir = Paths.get(value)
+  
+  check(Files.isDirectory(dir), {"System.getenv($this) should be a path pointing to an existing directory. " +
+      "The faulty path: ${dir.toString()}"})
+  
+  return dir
+}
---
+++ src/test/kotlin/com/github/konrad_jamrozik/extensionsTest.kt
@@ -0,0 +1,10 @@
+package com.github.konrad_jamrozik
+
+import org.junit.Test
+
+class ExtensionsTest {
+
+  @Test
+  fun KotlinScratchpadTest() {
+  }
+}
