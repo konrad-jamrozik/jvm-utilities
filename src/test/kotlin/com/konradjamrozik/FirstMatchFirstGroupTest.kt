cafb021162e58610b2981d46fd5c99e79959949f
Konrad Jamrozik <spawarotti@jamro.pl>
2016-12-06 01:04:45 +0100

    more powerful FirstMatchFirstGroup

---
+++ src/test/kotlin/com/konradjamrozik/FirstMatchFirstGroupTest.kt
@@ -0,0 +1,24 @@
+// Author: Konrad Jamrozik, github.com/konrad-jamrozik
+
+package com.konradjamrozik
+
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.`is`
+import org.junit.Test
+import kotlin.test.assertFailsWith
+
+class FirstMatchFirstGroupTest {
+
+  @Test
+  fun `matches`() {
+
+    assertFailsWith(Exception::class) { FirstMatchFirstGroup("target", "target")}
+    assertThat(FirstMatchFirstGroup("target", "(target)").value, `is`("target"))
+    assertThat(FirstMatchFirstGroup("target", "(target)", "target").value, `is`("target"))
+    assertThat(FirstMatchFirstGroup("target", "target", "(target)").value, `is`("target"))
+    
+    assertThat(FirstMatchFirstGroup("label='xyz'", "label='(.*)'").value, `is`("xyz"))
+    assertFailsWith(Exception::class) { FirstMatchFirstGroup("label=''", "label='(.*)'")}
+  }
+}
+  
