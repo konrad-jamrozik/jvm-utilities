cf8df7cffc86de73a395c4b378f38f83ca8fdcea
Konrad Jamrozik <spawarotti@jamro.pl>
2017-03-04 23:18:50 -0800

    edit scratchpad

--- src/test/kotlin/com/konradjamrozik/KotlinScratchpadTest.kt
+++ src/test/kotlin/com/konradjamrozik/KotlinScratchpadTest.kt
@@ -3,10 +3,13 @@
 package com.konradjamrozik
 
 import org.junit.jupiter.api.Test
+import java.nio.file.Paths
 
 internal class KotlinScratchpadTest {
 
   @Test
   fun MainTest() {
+    val dir = Paths.get("""""")
+    //dir.replaceTextInAllFiles("a","b")
   }
 }
