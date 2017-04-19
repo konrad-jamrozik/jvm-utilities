1fdcdfea51d32f4228c36a049a3fcfbc24d8b13a
Konrad Jamrozik <spawarotti@jamro.pl>
2017-04-18 23:54:30 -0700

    edit scratchpad

--- src/test/kotlin/com/konradjamrozik/KotlinScratchpadTest.kt
+++ src/test/kotlin/com/konradjamrozik/KotlinScratchpadTest.kt
@@ -2,8 +2,11 @@
 
 package com.konradjamrozik
 
+import com.google.common.base.Stopwatch
 import org.junit.jupiter.api.Test
+import java.io.FileWriter
 import java.nio.file.Paths
+import java.util.concurrent.TimeUnit
 
 internal class KotlinScratchpadTest {
 
@@ -11,5 +14,31 @@ internal class KotlinScratchpadTest {
   fun MainTest() {
     val dir = Paths.get("""""")
     //dir.replaceTextInAllFiles("a","b")
+  }
+
+  @Test
+  fun PerfTest() {
+    FileWriter("./temp.txt", false).use { file ->
+
+      val x = -1;
+      val stopwatch = Stopwatch.createStarted()
+      (1..100000).forEach {
+        file.write("this is log msg $it\n")
+      }
+      println(stopwatch.elapsed(TimeUnit.MILLISECONDS))
+      stopwatch.reset()
+      stopwatch.start()
+      (1..10000000).forEach {
+        invariant(x != it)
+      }
+      println(stopwatch.elapsed(TimeUnit.MILLISECONDS))
+
+    }
+  }
+
+  fun invariant(inv: Boolean) {
+    if (!inv) {
+      throw Exception()
+    }
   }
 }
